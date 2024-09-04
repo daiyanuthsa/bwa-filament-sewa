02c0a4d6b8da516f87ba25dfd80e74383fb50730
daiyanuthsa <95560026+daiyanuthsa@users.noreply.github.com>
2024-09-04 17:12:00 +0700

    add: set slug

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
@@ -24,6 +25,12 @@ class Product extends Model
     protected $casts = [
         'price' => MoneyCast::class,
     ];
+
+    public function setNameAttribute(string $value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 
     /**
      * Get the brand associated with the product.
--- app/Models/Store.php
+++ app/Models/Store.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
-
+use Illuminate\Support\Str;
 class Store extends Model
 {
     use HasFactory, SoftDeletes;
@@ -17,4 +17,10 @@ class Store extends Model
         'address',
         'is_open',
     ];
+
+    public function setNameAttribute(string $value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 }
